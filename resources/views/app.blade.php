8fe69704262e033186227ae72c251c71c057f44c
Daffa Mumtaz <daffamumtaz2001@gmail.com>
2020-06-16 11:20:16 +0700

    Laravel mix tweak

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Laravel</title>
+	<title>{{ env('APP_NAME') }}</title>
 </head>
 <body style="margin: 0; background-color: #eee;">
 	<div id="app"></div>
--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,6 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.sass('resources/sass/app.scss', 'public/css')
-   .react('resources/js/app.js', 'public/js')
+mix.react('resources/js/app.js', 'public/js')
    .extract()
    .sourceMaps();
